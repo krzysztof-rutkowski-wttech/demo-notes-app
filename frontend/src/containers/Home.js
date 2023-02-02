370e86ae0bfd91a428e83f0ce13fdd4c3818ea46
Krzysztof Rutkowski <krzysztof.rutkowski@wundermanthompson.com>
2023-02-02 13:56:21 +0100

    Fix naming issue

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -81,7 +81,12 @@ export default function Home() {
 
   return (
     <div className="Home">
-      {isAuthenticated ? renderNotes() : renderLander()}
+      {
+        isAuthenticated ? renderNotes() : renderLander()
+      }
+      {
+        isAuthenticated.none.no
+      }
     </div>
   );
 }
